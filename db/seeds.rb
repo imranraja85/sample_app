215cba23a087a1c70446050b5a761b989dd6c5f3
Imran Raja <imranraja85@gmail.com>
2015-10-23 21:32:44 -0400

    added single model and pg to get ready for container deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Task.create(name: 'Persist database across containers', completed: false)
+Task.create(name: 'Profit', completed: false)
